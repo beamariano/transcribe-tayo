65d8d3908370fffe9b249e299b4feb64bb7c8748
Bea M <64287361+beamariano@users.noreply.github.com>
2022-05-31 12:33:13 +0800

    feat: react player

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import { AspectRatio, Box } from "@chakra-ui/react";
 
 const MediaPlayer = () => {
-  return <div>MediaPlayer</div>;
+  const [mediaUrl, setMediaUrl] = useState("");
+
+  useEffect(() => {
+    setMediaUrl();
+  }, []);
+
+  return (
+    <Box w="450px">
+      <AspectRatio ratio={16 / 9}>
+        <ReactPlayer height="100%" url={mediaUrl} width="100%" />
+      </AspectRatio>
+    </Box>
+  );
 };
 
 export default MediaPlayer;
