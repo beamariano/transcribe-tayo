4340b626ded1efa5aedb0e46a79646e980c8e2e8
Bea M <64287361+beamariano@users.noreply.github.com>
2022-05-31 12:49:28 +0800

    fix: downgrade react etc

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import HomeScreen from "../src/components/HomeScreen";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <HomeScreen />
-    </div>
-  );
+  return <HomeScreen />;
 }
--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
-import { AspectRatio, Box } from "@chakra-ui/react";
+import { AspectRatio, Box, Center } from "@chakra-ui/react";
 
 const MediaPlayer = () => {
-  const [mediaUrl, setMediaUrl] = useState("");
+  const [mediaUrl, setMediaUrl] = useState(
+    "https://www.youtube.com/watch?v=-mIA1r2ZELU"
+  );
 
-  useEffect(() => {
-    setMediaUrl();
-  }, []);
+  // useEffect(() => {
+  //   setMediaUrl("https://www.youtube.com/watch?v=-mIA1r2ZELU");
+  // }, []);
 
   return (
-    <Box w="450px">
-      <AspectRatio ratio={16 / 9}>
-        <ReactPlayer height="100%" url={mediaUrl} width="100%" />
-      </AspectRatio>
+    <Box w="450px" margin="0 auto">
+      <Center>
+        <Box w="450px">
+          <AspectRatio ratio={16 / 9}>
+            <ReactPlayer height="100%" url={mediaUrl} width="100%" />
+          </AspectRatio>
+        </Box>
+      </Center>
     </Box>
   );
 };
